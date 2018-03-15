8e4e61dee81bf0a70e293a172ada8e7cb8945905
Valentin Roland <valentin@vroland.de>
2018-03-15 23:02:22 +0100

    enable CORS header to allow cross-site access

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -4,17 +4,23 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
-extern crate rocket_contrib;
+extern crate serde_json;
 extern crate mwlint;
 extern crate mediawiki_parser;
 #[macro_use]
 extern crate serde_derive;
+extern crate serde;
 
 use mediawiki_parser::*;
 use mwlint::*;
+use serde::{Serialize};
+use serde_json::Value;
 
+use rocket::http::Status;
 use rocket::request::{Form};
-use rocket_contrib::{Json};
+use rocket::response::{self, Responder, content};
+use rocket::http::Header;
+use rocket::request::Request;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -30,6 +36,25 @@ struct SourceForm {
 enum ResultKind {
     Lints(Vec<Lint>),
     Error(MWError),
+}
+
+
+#[derive(Debug)]
+pub struct Json<T = Value>(pub T);
+
+/// Added CORS headers to JSON contrib implementation.
+impl<T: Serialize> Responder<'static> for Json<T> {
+    fn respond_to(self, req: &Request) -> response::Result<'static> {
+        serde_json::to_string(&self.0).map(|string| {
+            {
+                let mut rsp = content::Json(string).respond_to(req).unwrap();
+                rsp.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+                rsp
+            }
+        }).map_err(|_e| {
+            Status::InternalServerError
+        })
+    }
 }
 
 #[post("/", data = "<source_form>")]
@@ -50,6 +75,7 @@ fn lint(source_form: Form<SourceForm>) -> Json<ResultKind> {
             .expect("error while checking rule!");
         lints.append(&mut rule.lints().iter().map(|l| l.clone()).collect())
     }
+
     Json(ResultKind::Lints(lints))
 }
 
