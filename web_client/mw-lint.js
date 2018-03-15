69f1030aeaa3b9da874fa90c6e92ab4b586dcaa1
Valentin Roland <valentin@vroland.de>
2018-03-15 21:23:47 +0100

    get rid of jquery dependency and some styling

--- web_client/mw-lint.js
+++ web_client/mw-lint.js
@@ -15,22 +15,36 @@
 })(function(CodeMirror) {
 "use strict";
 
+function cmp_severity(a, b) {
+    var values = {
+        info: 10,
+        warning: 20,
+        error: 30
+    };
+    return values[b.severity] - values[a.severity];
+}
+
 CodeMirror.registerHelper("lint", "markdown", function(text, options) {
   var found = [];
+
   if (last_lints === null || last_lints === undefined) {
     console.run("last lints is undefined, can't show lints in editor!");
     return found;
   }
 
+  last_lints.sort(cmp_severity);
+
   for ( var i = 0; i < last_lints.length; i++) {
     var lint = last_lints[i];
-    console.log(lint);
     var severity = lint.severity;
-
+    console.log(lint);
     found.push({
       from: CodeMirror.Pos(lint.position.start.line - 1, lint.position.start.col - 1),
       to: CodeMirror.Pos(lint.position.end.line - 1, lint.position.end.col - 1),
-      message: lint.explanation + "\n=> try: " + lint.solution,
+      messageHTML: "<span class=\"explanation explanation-" + lint.severity + 
+        "\">" + lint.explanation + "</span>" + 
+        "<br><span class=\"solution\">" + lint.solution + "</span>" + 
+        "<br><span class=\"explanation_long\">" + lint.explanation_long + "</span>",
       severity : severity
     });
   }
